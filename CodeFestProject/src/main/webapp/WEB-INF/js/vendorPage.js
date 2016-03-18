f48dcf42f6480088adaf8fe6de233b5d30cc070e
boopatrx <r.boopathy@elsevier.com>
2016-03-18 07:06:30 -0400

    Traction version of vendorpage

--- CodeFestProject/src/main/webapp/WEB-INF/js/vendorPage.js
+++ CodeFestProject/src/main/webapp/WEB-INF/js/vendorPage.js
@@ -2,8 +2,57 @@ $( document ).ready(function() {
 	var responseObj;
 	var menuID;
 	getMenuDetails();
+	$('.tranctionListLinks').click(function (e) {
+		getTranscationDetails();
+	});
+	$('.menuListLinks').click(function (e) {
+		getMenuDetails();
+	});
 });
+function getTranscationDetails() {
+	$("#menuItems").hide();
+	$("#tansactionDetails").show();
+	$.ajax({
+		  url: "/vendor",
+		  dataType: "text",
+		  type:'get',
+		  success: function(data){
+			  responseObj=JSON.parse(data);
+			  createTransactionTable(responseObj);
+		  }
+		});
+}
+function createTransactionTable(transactiondetails) {
+	var tabMainCont=jQuery('#example');
+	var rowCount = jQuery('#example tr').length;
+	if(rowCount > 0) {
+		jQuery('#example tr').remove();
+	}
+	var tableHeaderCont=jQuery('<thead> <tr><th>Transaction ID</th><th>Menu Name</th><th>quantity</th><th>Price</th><th>Delivered</th></tr></thead>');
+	tabMainCont.append(tableHeaderCont);
+	var tableListCont=jQuery('<tbody>');
+	var iVal = 1; 
+	$.each(transactiondetails,function(i,obj){
+		var tableRow=$('<tr id=MmenuTableRow'+iVal+'>');
+		var tableData=$('<td>').text(obj.menuId).appendTo(tableRow);
+		var tableData=$('<td>').text(obj.menuName).appendTo(tableRow);
+		var tableData=$('<td>').text(obj.menuDescription).appendTo(tableRow);
+		var tableData=$('<td>').text(obj.price).appendTo(tableRow);
+		var tableData=$('<td>').text(obj.availability).appendTo(tableRow);
+		tableListCont.append(tableRow);
+		iVal++;
+	  });
+	  tabMainCont.append(tableListCont);
+	  rowCount = jQuery('#example tr').length;
+	  if(rowCount > 0) {
+			$('#example_info').text('Showing 1 to '+(rowCount-1)+' of 12 entries');
+		} else {
+			$('#example_info').text('Showing 0 result');
+		}
+}
 function getMenuDetails(){
+	$("#tansactionDetails").hide();
+	$("#menuItems").show();
 	$.ajax({
 		  url: "/vendor",
 		  dataType: "text",
@@ -22,7 +71,7 @@ function createMenuTable(menuDetails) {
 		jQuery('#example1 tr').remove()
 	}
 
-	  var tableHeaderCont=jQuery('<thead> <tr><th>Menu ID</th><th>Dish Name</th><th>Description</th> <th>Price</th><th>Quantity</th><th>Active</th><th>Actions</th></tr></thead>');
+	  var tableHeaderCont=jQuery('<thead> <tr><th>Menu ID</th><th>Dish Name</th><th>Description</th> <th>Price</th><th>Quantity</th><th>Availability</th><th>Actions</th></tr></thead>');
 	  tabMainCont.append(tableHeaderCont);
 	  var tableListCont=jQuery('<tbody>');
 	  var iVal = 1; 
@@ -32,7 +81,7 @@ function createMenuTable(menuDetails) {
 			var tableData=$('<td>').text(obj.menuName).appendTo(tableRow);
 			var tableData=$('<td>').text(obj.menuDescription).appendTo(tableRow);
 			var tableData=$('<td>').text(obj.price).appendTo(tableRow);
-			var tableData=$('<td>').text("100").appendTo(tableRow);
+			var tableData=$('<td>').text(obj.quantity).appendTo(tableRow);
 			var tableData=$('<td>').text(obj.availability).appendTo(tableRow);
 			var tableData=$('<td>').append("<a href='#updateMenu' id='menuUpdate' role='button' data-toggle='modal' class='edit-menu'><img src='images/edit-menu.png' alt='Edit' /></a> " +
 					"<a  href='#deleteMenu' role='button' data-toggle='modal' class='delete-menu'><img src='images/delete-menu.png' alt='Delete' /></a>").appendTo(tableRow);
@@ -72,7 +121,7 @@ function updateMenuDetails(event) {
 		  success: function(data){
 		  }
 		});
-	$('#updateMenu').modal('hide');
+	$('#updateMenu').hide();
 	getMenuDetails();
 }
 
